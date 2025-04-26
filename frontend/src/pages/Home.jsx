c49f5017e082698359aff2aac4ca7bc0bc19575d
nurul <95nurulislam786@gmail.com>
2025-04-26 23:50:39 +0530

    commit files via git

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import TrelloBoard from "../components/TaskBoard ";
+import Footer from "../components/Footer";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <TrelloBoard />
+      <Footer />
+    </div>
+  );
+};
+export default Home;
